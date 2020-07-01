ad55ee71a7c89e6dea96a8359cf8490e321053e1
linus <738435894@qq.com>
2020-07-01 23:27:26 +0800

    Initial commit

---
+++ src/com/linus/Hello.java
@@ -0,0 +1,30 @@
+package com.linus;
+
+import com.sun.xml.internal.bind.v2.runtime.output.StAXExStreamWriterOutput;
+import jdk.nashorn.tools.Shell;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author linus
+ * @create 2020-07-01 18:11
+ */
+public class Hello {
+
+    public static void main(String[] args) {
+//        System.out.println();
+        List<String> list = new ArrayList<>();
+        list.add("aaa");
+        list.add("bbb");
+        list.add("ccc");
+        list.add("ddd");
+
+
+    }
+
+    
+    public void testFindAll() {
+        
+    }
+}
